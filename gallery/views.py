cac901e2184f944ab70c321261b32ffb5ad68ee3
Saransh Sood <saranshsood24@gmail.com>
2024-01-18 23:06:22 +0530

    Adding images partway done

--- gallery/views.py
+++ gallery/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from django.core.files.storage import default_storage
+from django import forms
+from PIL import Image
+import cv2, os
+class ImageUpload(forms.Form):
+    image = forms.FileField(label="Upload an image")
+    alt = forms.CharField(label="Enter alt text")
 
 
 # Create your views here.
@@ -6,4 +13,19 @@ def index(request):
     return render(request, "gallery/index.html")
 
 def create(request):
-    return render(request, "gallery/create.html")
+    if request.method == "POST":
+        image = request.POST.get("image")
+        alt = request.POST.get("alt")
+        dir = "gallery/static/images/"
+        file = cv2.imread(image)
+        os.chdir(dir)
+        cv2.imwrite(image, file)
+        # filename = f"images/{image}"
+        # default_storage.save(image, image)
+        return render(request, "gallery/index.html", {
+            "image" : image,
+            "alt" : alt
+        })
+    return render(request, "gallery/create.html", {
+        "image" : ImageUpload()
+    })
