ac9b33834bfc8acc1a32bb0ba9ce3b3ed579476a
Saransh Sood <saranshsood24@gmail.com>
2024-02-05 23:52:55 +0530

    Add randomization and remove DRY principle

--- squishy/views.py
+++ squishy/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 
+import random
 
+def annotate_tiles(n):
+    tile_map = [i for i in range(n)]
+    random.shuffle(tile_map)
+
+    for i, val in enumerate(tile_map):
+        if (val % 2 == 0) or (val % 7 == 0):
+            tile_map[i] = "small"
+        elif (val % 5 == 0) or (val % 3 == 0):
+            tile_map[i] = "medium"
+        else:
+            tile_map[i] = "large"
+    return tile_map
 # Create your views here.
 def index(request):
-    return render(request, "squishy/index.html")
+    return render(request, "squishy/index.html", {
+        "map" : annotate_tiles(100)
+    })
