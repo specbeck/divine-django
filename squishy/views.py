e522036d3065df0075b3ebd916ac533fe7de27f6
Saransh Sood <saranshsood24@gmail.com>
2024-02-07 23:52:37 +0530

    Code refactor

--- squishy/views.py
+++ squishy/views.py
@@ -2,20 +2,32 @@ from django.shortcuts import render
 
 import random
 
-def annotate_tiles(n):
-    tile_map = [i for i in range(n)]
-    random.shuffle(tile_map)
+def generate_tiles(n):
+    types = ["vlarge", "large", "medium", "small", "vsmall"]
+    weights = [0.1, 0.25, 0.3, 0.25, 0.1]
+    tiles = random.choices(population=types, weights=weights, k=n)
+    random.shuffle(tiles)
+    return tiles
 
-    for i, val in enumerate(tile_map):
-        if (val % 2 == 0) or (val % 7 == 0):
-            tile_map[i] = "small"
-        elif (val % 5 == 0) or (val % 3 == 0):
-            tile_map[i] = "medium"
-        else:
-            tile_map[i] = "large"
-    return tile_map
+# def annotate_tiles(n):
+#     tile_map = [i for i in range(n)]
+#     random.shuffle(tile_map)
+
+#     for i, val in enumerate(tile_map):
+#         if (val % 2 == 0):
+#             tile_map[i] = "vsmall"
+#         elif (val % 3 == 0):
+#             tile_map[i] = "small"
+#         elif (val % 5 == 0):
+#             tile_map[i] = "medium"
+#         elif (val % 7 == 0):
+#             tile_map[i] = "vlarge"
+#         else:
+#             tile_map[i] = "large"
+#     return tile_map
+
 # Create your views here.
 def index(request):
     return render(request, "squishy/index.html", {
-        "map" : annotate_tiles(100)
+        "map" : generate_tiles(100)
     })
